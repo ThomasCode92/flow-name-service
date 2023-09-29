855234643c1cfbf29ecfd34a65c7aef5c1c22afc
ThomasCode92 <thomascode92@hotmail.com>
2023-09-29 16:37:07 +0200

    Setting Up Next.js Project

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Flow Name Service</title>
+        <meta name="description" content="Decentralized Naming Service" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </>
+  );
+}
